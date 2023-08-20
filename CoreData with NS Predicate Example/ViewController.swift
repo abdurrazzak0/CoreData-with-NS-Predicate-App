55e1d12a5f40e0a6ef8029459c4188c855c693dc
abdurrazzak0 <razzak6366@gmail.com>
2023-08-20 14:15:48 +0600

    Initial Commit

---
+++ CoreData with NS Predicate Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData with NS Predicate Example
+//
+//  Created by Abdur Razzak on 20/8/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
